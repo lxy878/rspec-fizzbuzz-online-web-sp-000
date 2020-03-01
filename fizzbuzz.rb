02351030fabd4284f1b06de70bca99e5c483ffff
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 04:03:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,10 @@
 # See README.md for instructions on how to do this
 required "fizzbuzz_spec.rb"
 def fizzbuzz(number)
-
+  result = nil
+  if number%3 == 0
+    result += "Fizz"
+  if number%5 == 0
+    result += "Buzz"
+  return result
 end

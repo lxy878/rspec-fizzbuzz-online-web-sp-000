9e9d54d61a6e08f182ba361ac2ab9d3c424229a3
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 04:04:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-required "fizzbuzz_spec.rb"
+
 def fizzbuzz(number)
   result = nil
   if number%3 == 0
@@ -10,7 +10,7 @@ def fizzbuzz(number)
   if number%5 == 0
     result += "Buzz"
   end
-  
+
   return result
 end
 

26472895680a705a67d8b09c11b01df338dab3d3
Xiaoyi Liu <lxy878@gmail.com>
2020-03-01 04:47:17 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require './fizzbuzz.rb'
+
+def fizzbuzz(number)
+  result = ""
+  if 15%number == 0
+    if number%3 == 0
+      result << "Fizz"
+    end
+
+    if number%5 == 0
+      result << "Buzz"
+    end
+
+  else
+    result = nil
+  end
+
+  return result
+end
